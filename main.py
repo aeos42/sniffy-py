a31492eb2ab7bcb6ce21b58e3efcdceeb723ef68
Adam Siefkas <asiefkas@Adams-MacBook-Air.local>
2021-04-26 22:28:35 -0600

    mocked out three endpoints

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 
 # PM1, 2.5, 10
 # temp, humidity, c02
+
+
 @app.route('/temperature')
 def get_temperature():
     string_datetime = str(datetime.datetime.now().isoformat())
     temp = {string_datetime: 68.2}
     return temp
+
 
 @app.route('/humidity')
 def get_humidity():
@@ -19,5 +22,9 @@ def get_humidity():
     return humidity
 
 
-    
-
+@app.route('/PM25')
+def get_pm25():
+    # micrometers per cubic meter?
+    string_datetime = str(datetime.datetime.now().isoformat())
+    pm25 = {string_datetime: 73.2}
+    return pm25
